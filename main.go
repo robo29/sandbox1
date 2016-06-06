983b3b8cc159dbe745c8c7e6abdd025f76a5dc47
Greg Curfman <greg.curfman@gmail.com>
2016-06-06 09:33:03 -0400

    webhook poking, iteration 3.

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+const iteration = 3
+
 func main() {
-	fmt.Printf("This is a Hello World Test!!\n")
+	fmt.Printf("This is a Hello World Test, interation %d.\n", iteration)
 	fmt.Printf("%s\n", hello())
 }
 
